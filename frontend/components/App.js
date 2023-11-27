e8ef7b8a78a28c8e31c20b57c213eb0fc62b00a6
Mitch Hammond <mitch.b.hammond@gmail.com>
2023-11-27 13:33:37 -0700

    final changes

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -3,6 +3,7 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import* as yup from 'yup'
+import { javascript } from 'webpack'
 
 const e = { // This is a dictionary of validation error messages.
   // username
@@ -19,6 +20,21 @@ const e = { // This is a dictionary of validation error messages.
   agreementRequired: 'agreement is required',
   agreementOptions: 'agreement must be accepted',
 }
+
+const userSchema = yup.object().shape({
+  username: yup.string().trim()
+    .required(e.usernameRequired)
+    .min(3, e.usernameMin).max(20, e.usernameMax),
+  favLanguage: yup.string()
+    .required(e.favLanguageRequired).trim()
+    .oneOf(['javascript', 'rust'], e.favLanguageOptions),
+  favFood: yup.string()
+    .required(e.favFoodRequired).trim()
+    .oneOf(['broccoli', 'spaghetti', 'pizza'], e.favFoodOptions),
+  agreement: yup.boolean()
+    .required(e.agreementRequired)
+    .oneOf([true], e.agreementOptions),
+})
 
 // ✨ TASK: BUILD YOUR FORM SCHEMA HERE
 // The schema should use the error messages contained in the object above.
@@ -47,7 +63,9 @@ const [serverSuccess, setServerSuccess] = useState()
 const [serverFailure, setServerFailure] = useState()
 const [formEnabled, setFormEnabled] =useState(false)
 
-
+useEffect(() => {
+  userSchema.isValid(values).then(setFormEnabled)
+}, [values])
   // ✨ TASK: BUILD YOUR EFFECT HERE
   // Whenever the state of the form changes, validate it against the schema
   // and update the state that tracks whether the form is submittable.
@@ -61,6 +79,9 @@ const [formEnabled, setFormEnabled] =useState(false)
     let {type, name, value, checked } = evt.target
     value = type == 'checkbox' ? checked : value
     setValues({...values, [name]: value })
+    yup.reach(userSchema, name).validate(value)
+    .then(() => setErrors({...errors, [name]: ''}))
+    .catch((err) => setErrors({...errors, [name]: err.errors[0]}))
   }
 
   const onSubmit = evt => {
